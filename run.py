5663e8caae684aadbc7009ec0e02c0f0157246be
MatiMar <mateusz030100@o2.pl>
2022-05-18 19:14:08 +0200

    added gui

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ import numpy as np
 import genetic_algorithm as ga
 import random_grid_generator as rgg
 
-import tkinter
+import tkinter as tk
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
+from tkinter import ttk
 # # # Implement the default Matplotlib key bindings.
 # # from matplotlib.backend_bases import key_press_handler
 # from matplotlib.figure import Figure
@@ -23,10 +24,16 @@ def use_alg(r_grid):
     ga_solution = np.reshape(ga_solution, (ga_solution.size,))
     return ga_solution
 
+def jaka():
+    pass
+
+
+
+
 
 def main():
     # Minimum and maximum number of electrical buses in a randomly generated power grid model.
-    min_buses, max_buses = 10, 12
+    min_buses, max_buses = 40, 50
 
     # Generating a random power system model.
     random_grid = rgg.RandomPowerGrid(min_buses, max_buses)
@@ -35,17 +42,24 @@ def main():
     ga_sol = []
 
     # Using a genetic algorithm.
-    ga_sol = use_alg(random_grid)
+    # ga_sol = use_alg(random_grid)
     no_pmu = list(set(random_grid.nodes) - set(ga_sol))
 
+    # tkinter gui
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.geometry('800x600')
+    root.title('Image Button Demo')
 
-    window = tkinter.Tk()
+
+
     fig = plt.figure(0)
-    canvas = FigureCanvasTkAgg(fig, window)
-    canvas.get_tk_widget().grid(row=1, column=1)
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    canvas = FigureCanvasTkAgg(fig, root)
+    canvas.get_tk_widget().pack()
 
     # Drawing nodes
-    pos = nx.spring_layout(random_grid)
+    pos = nx.spring_layout(random_grid, seed=1234)
     nx.draw_networkx_nodes(random_grid, nodelist=ga_sol, node_color="green", pos=pos)
     nx.draw_networkx_nodes(random_grid, nodelist=no_pmu, node_color="red", pos=pos)
 
@@ -56,6 +70,48 @@ def main():
     labels_kv = list(random_grid.nodes)
     labels = {labels_kv[i]: labels_kv[i] for i in range(len(labels_kv))}
     nx.draw_networkx_labels(random_grid, labels=labels, pos=pos)
+
+
+
+    # chaos Buttons
+    # wybór min i max
+    # losuj
+    def rozpierdol():
+        ga_sol = use_alg(random_grid)
+        no_pmu = list(set(random_grid.nodes) - set(ga_sol))
+        ax.clear()
+        canvas.get_tk_widget().pack_forget()
+
+        # Drawing nodes
+        pos = nx.spring_layout(random_grid, seed=1234)
+        nx.draw_networkx_nodes(random_grid, nodelist=ga_sol, node_color="green", pos=pos)
+        nx.draw_networkx_nodes(random_grid, nodelist=no_pmu, node_color="red", pos=pos)
+
+        # Drawing power grid model with electrical buses labels.
+        nx.draw_networkx_edges(random_grid, pos=pos)
+
+        # Drawing labels.
+        labels_kv = list(random_grid.nodes)
+        labels = {labels_kv[i]: labels_kv[i] for i in range(len(labels_kv))}
+        nx.draw_networkx_labels(random_grid, labels=labels, pos=pos)
+        canvas.get_tk_widget().pack()
+
+    # odpal algorytm
+    optimize_button = ttk.Button(
+        master=root,
+        text="optymalizuj",
+        command=rozpierdol,
+        compound=tk.BOTTOM
+    )
+    optimize_button.pack()
+
+
+
+
+
+
+
+    root.mainloop()
 
 
 if __name__ == '__main__':
